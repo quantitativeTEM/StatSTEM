60e02a91adba6da966249c214130ec52185b742e
quantitativeTEM <Karel.vandenBos@uantwerpen.be>
2016-11-21 16:21:34 +0100

    Add files via upload

--- Examples/runFitProgramManual.m
+++ Examples/runFitProgramManual.m
@@ -5,13 +5,13 @@ pathF = '..\functions';
 addpath(genpath(pathF))
 
 %% Fit Options
-FP.fitzeta = 1;    % Fit background
+FP.fitzeta = 0;    % Fit background
 FP.zeta = 0;       % Value background (if not fitted)
 FP.widthtype = 1;  % Use same Gaussian width for different atomic columns (1 = 'same', 0 = 'different')
 FP.atomsToFit = 1; % Fit Gaussian function to all selected peak positions
 FP.test = 0;       % Will fit be a test (1 = 'yes', 0 = 'no')
 FP.cluster = 1;    % Calculation in a cluster, print progress in command window (1 = 'yes', 0 = 'no')
-FP.maxIter = 1;  % Maximum number of iterations
+FP.maxIter = 400;  % Maximum number of iterations
 FP.GUI = 0;        % Use GUI (1 = 'yes', 0 = 'no')
 FP.numWorkers = feature('numCores'); % Number of cores for parallel computing
 FP.silent = 1;     % Hide progress indication from comment window
@@ -26,10 +26,34 @@ if str2double(v(1:4))<2015
 end
 
 %% Select file(s)
-[filename, pathname] = uigetfile('*.mat','MultiSelect','on');
-if ischar(filename)
-    filename = {filename};
+% [filename, pathname] = uigetfile('*.mat','MultiSelect','on');
+% if ischar(filename)
+%     filename = {filename};
+% end
+pathnames = uigetfiles;
+filename = cell(0,0);
+for n=1:length(pathnames)
+    files = dir(fullfile(pathnames{n}));
+    names = cell(1,length(files));
+    for m=1:length(files)
+        [pathstr,name,ext] = fileparts(pathnames{n});
+        if strcmp(ext,'.mat')
+            names{1,m} = [pathstr,filesep,files(m).name];
+        else
+            [pathstr,name,ext] = fileparts([pathnames{n},filesep,files(m).name]);
+            if strcmp(ext,'.mat')
+                names{1,m} = [pathstr,filesep,files(m).name];
+            else
+                names{1,m} = '';
+            end
+        end
+    end
+    if isempty(names{1,1})
+        names = names(3:end);
+    end
+    filename = [filename,names];
 end
+pathname = '';
 
 %% Run program
 % If multiple files selected run fitProgram on all files
